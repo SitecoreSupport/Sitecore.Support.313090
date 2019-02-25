695937b3198505f8234c74404f7d4e0f3a773dda
DK\ELB <elb@sitecore.net>
2019-02-25 16:33:53 +0200

    no message

---
+++ src/Sitecore.Support.313090/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.313090")]
+[assembly: AssemblyProduct("Sitecore.Support.313090")]
+[assembly: ComVisible(false)]
